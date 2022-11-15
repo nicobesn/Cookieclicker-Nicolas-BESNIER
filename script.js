c05315203553e519d6a390532376567f4c41819f
nicobesn <95340756+nicobesn@users.noreply.github.com>
2022-11-15 14:32:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var HTML_image = document.querySelector("#image");
 var clickers = 0;
 var autoclicker_cost = 1000;
 var multiplicateur_cost = 10;
-var multi5_cost = 100; 
+var multi5_cost = 100;
 var change_click;
 var bpc = 1;
 var bpc1 = 0;
@@ -53,7 +53,7 @@ window.setInterval(play_autoclickers, 1000);
 function play() {
     score = score + bpc;
     score = score + bpc1;
-    score = score + bpc2;   
+    score = score + bpc2;
     HTML_berry.textContent = Math.round(score);
 }
 
@@ -64,7 +64,7 @@ function augmenterMULTIPLIER() {
     }
     else {
         score -= multiplicateur_cost;
-        bpc1 = Math.round(bpc1+ 1);
+        bpc1 = Math.round(bpc1 + 1);
         multiplicateur_cost = Math.round(multiplicateur_cost * 2);
         document.getElementById('multiplicateur_cost').innerHTML = multiplicateur_cost;
     }
@@ -77,7 +77,7 @@ function multi5() {
     }
     else {
         score -= multi5_cost;
-        if (bpc2 == 0) { bpc2 = 1}
+        if (bpc2 == 0) { bpc2 = 1 }
         bpc2 = Math.round(bpc2 * 5)
         multi5_cost = Math.round(multi5_cost * 5);
         document.getElementById('multi5_cost').innerHTML = multi5_cost;
@@ -119,23 +119,23 @@ function changement_image() {
     }
     else if (score > 45000000 && score < 1000000000) {
         nouvelle_url = "image/gear4-boundman.png"
-        if(!gear4.ended) {
+        if (!gear4.ended) {
             gear4.play()
         }
     }
     else if (score > 1000000000 && score < 1200000000) {
         nouvelle_url = "image/gear4-tankman.png"
-        if(!gear4tank.ended) {
+        if (!gear4tank.ended) {
             gear4tank.play()
         }
     }
     else if (score > 1200000000 && score < 1500000000) {
         nouvelle_url = "image/gear4-snakeman.png"
-        if(!gear4snake.ended) {
+        if (!gear4snake.ended) {
             gear4snake.play()
         }
     }
-    else if (score >3000000000) {
+    else if (score > 3000000000) {
         nouvelle_url = "image/gear5.png"
     }
 
